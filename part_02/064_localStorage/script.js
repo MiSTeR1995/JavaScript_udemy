d2ae84ad6bd261c67c9525e1ce006d3e4dc635f8
Alex <36799721+MiSTeR1995@users.noreply.github.com>
2020-12-24 17:59:46 +0300

    local storage upd

--- part_02/064_localStorage/script.js
+++ part_02/064_localStorage/script.js
@@ -19,7 +19,7 @@ localStorage.removeItem('key');
 console.log(localStorage.getItem('key'));
 
 // очистить хранилище clear
-localStorage.clear();
+// localStorage.clear();
 
 const checkbox = document.querySelector('#checkbox');
 const form = document.querySelector('form');
@@ -27,12 +27,48 @@ const change = document.querySelector('#color');
 
 // поработаем с хранилищем чтобы сохранять настройки
 
-// проверим локальное хранилище на наличие ключа и нужного значение (в данном случае true)
-if(localStorage.getItem('isChecked')) {
+// проверим локальное хранилище на наличие ключа
+// если оно было true, то назначим его
+if (localStorage.getItem('isChecked')) {
     checkbox.checked = true;
 }
 
+// проверить запись в локал сторадже на запись про кнопку
+if (localStorage.getItem('bg') === 'changed') {
+    form.style.backgroundColor = 'red';
+}
+
+// событие change отрабатывает на изменение чекбокса
 checkbox.addEventListener('change', () => {
     // изменяем хранилище при клике на галочку
     localStorage.setItem('isChecked', true);
 });
+
+change.addEventListener('click', () => {
+    if (localStorage.getItem('bg') === 'changed') {
+        localStorage.removeItem('bg');
+        // можно вместо удаления ключа менять его на пустую строку
+        form.style.backgroundColor = '#fff';
+    } else {
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = 'red';
+    }
+});
+
+// в локальном хранилище мы можем хранить абсолютно любые настройки
+// туда можно записывать и объекты и массивы
+
+// но их нужно будет перевести в приемлемый формат, например json
+// например данные о опользователе
+const persone = {
+    name: 'alex',
+    age: 25
+};
+
+// сохраним его в локальное хранилище
+// если записать его напрямую, то вместо нужного объекта будет надпись object\
+// поэтому переведем для начала его в json
+const serializedPersone = JSON.stringify(persone);
+
+localStorage.setItem('alex', serializedPersone);
+console.log(localStorage.getItem('alex'));
